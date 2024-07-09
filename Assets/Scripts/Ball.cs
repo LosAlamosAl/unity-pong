10eefb0fc14dda8abd34def8528247f1a7cc04fa
Allen McPherson <al@losalamosal.me>
2024-07-08 20:01:42 -0600

    Predict and display ball's future path
    This doesn't work fully. Need to catch either collision
    with paddle or left wall and restart the predictions.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Ball : MonoBehaviour
-{
+public class Ball : MonoBehaviour {
     private Rigidbody2D _rb;
-    private float _speed = 200.0f;
+    private readonly float _speed = 200.0f;
     private int _framesSince;
     private Vector3 _lastPos;
+    private bool _doRaycast = true;
 
     void Awake() {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    void Start()
-    {
+    void Start() {
         _framesSince = 0;
-        _lastPos = transform.position;
         AddStartingForce();
+        _lastPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        var dir = (transform.position - _lastPos) * 5;
-        Debug.DrawRay(transform.position, dir, Color.green, 1, false);
+    void FixedUpdate() {
+        if (_framesSince != 0 && _doRaycast) {
+            DrawProjectedPath();
+            _doRaycast = false;
+        }
+        _framesSince++;
         _lastPos = transform.position;
     }
 
@@ -34,5 +36,27 @@ public class Ball : MonoBehaviour
                                         Random.Range( 0.5f,  1.0f);
 
         _rb.AddForce(new Vector2(x, y) * _speed);
+    }
+
+    private void DrawProjectedPath() {
+        var dir = (transform.position - _lastPos).normalized;  // initial direction
+        var origin = _lastPos + dir;  // initial ray origin (fudged outside ball)
+        RaycastHit2D hit;
+        do {
+            hit = Physics2D.Raycast(origin, dir);
+            if (hit.collider == null) break; 
+            PrintHitInfo(hit);
+            Debug.DrawLine(origin, hit.point, Color.yellow, 5);
+            dir = Vector2.Reflect(dir, hit.normal);
+            origin = hit.point + ((Vector2)dir * .01f);  // fudge (fix this)
+        } while (hit.collider.name != "LeftWall");
+    }
+
+    private void PrintHitInfo(RaycastHit2D hit) {
+        var debug = "HIT!\n";
+        debug += $"{hit.collider.name}\n";
+        debug += $"{hit.point}\n";
+        debug += $"{hit.normal}\n";
+        print(debug);
     }
 }
